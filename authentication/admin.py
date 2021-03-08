43a9d063221ebe325d354a014175d8969e3fdde9
d-kimsuon <d-kimuson@gmail.com>
2021-03-08 20:02:13 +0900

    [add] added authentiaction app

---
+++ authentication/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'last_login')}),
+        ('Permissions', {'fields': (
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+        )}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('email', 'password1', 'password2')}),
+    )
+
+    list_display = ('pk', 'email', 'is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser',)
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, UserAdmin)
